40a723db501203d14d68cf4340828e114b31366e
ellijayne <ellijayne.m.willard@gmail.com>
2018-05-17 10:39:49 +1000

    created game logic non test ver

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardTwo);
+//console.log("User Flipped " + cardOne);
+//console.log("User Flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	result = alert("You found a match!");
+} else if (cardsInPlay.length === 2 && cardsInPlay [0] !== cardsInPlay[1]) {
+	result = alert("Sorry. Try again.");
+}
