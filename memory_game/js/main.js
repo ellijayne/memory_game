87e2bcf1f1e7ea6d7a68d37018aa1f16342cb7ff
ellijayne <ellijayne.m.willard@gmail.com>
2018-05-21 15:51:49 +1000

    created further game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,14 +3,25 @@ console.log("Up and running!");
 //console.log("User Flipped " + cardTwo);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+var checkForMatch = function(){
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	result = alert("You found a match!");
 } else if (cardsInPlay.length === 2 && cardsInPlay [0] !== cardsInPlay[1]) {
 	result = alert("Sorry. Try again.");
 }
+};
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+
+};
+flipCard(0);
+flipCard(2);
+
+
+
+
+
